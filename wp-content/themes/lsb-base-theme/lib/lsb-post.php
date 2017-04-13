8362bd295bee26c3c407428d90ab9db3b316af62
Benedicte Raae <raae@bgraphic.no>
2017-04-13 23:20:49 +0200

    Transient caching of sections

--- wp-content/themes/lsb-base-theme/lib/lsb-post.php
+++ wp-content/themes/lsb-base-theme/lib/lsb-post.php
@@ -5,7 +5,7 @@ class LSB_Post extends TimberPost {
 	var $_read_more;
 	var $_sections;
 
-	public function lsb_read_more() {
+	public function read_more() {
 		if( !$this->_read_more ) {
 			$post_type_obj = get_post_type_object( $this->post_type );
 			if(isset($post_type_obj->labels->lsb_read_more)) {
@@ -18,22 +18,25 @@ class LSB_Post extends TimberPost {
 		return $this->_read_more;
 	}
 
-	public function lsb_sections() {
+	public function sections() {
 		if( !$this->_sections ) {
 			$this->_sections = get_field('lsb_post_sections');
 			if( !$this->_sections) {
 				$this->_sections = array();
 			}
 
+			$modified = get_the_modified_date( 'U', $this );
+
 			foreach ($this->_sections as $key => &$section) {
 				$post_type = $section['acf_fc_layout'];
-				$section['post_type'] = $post_type;
-
 				if(post_type_exists($post_type)) {
+					$section['post_type'] = $post_type;
 					$section['title'] = $section['lsb_title'];
 					$section['link'] = get_post_type_archive_link($post_type);
 					$section['subtitle'] = $section['lsb_subtitle'];
-					$section['posts'] = Timber::get_posts(array('post_type' => $post_type), LSB_Post::class);
+					$section['posts'] = TimberHelper::transient($post_type.$modified, function()  use ($post_type, $modified) {
+						return Timber::get_posts(array('post_type' => $post_type), LSB_Post::class);
+					}, 600);
 				}
 			}
 		}
