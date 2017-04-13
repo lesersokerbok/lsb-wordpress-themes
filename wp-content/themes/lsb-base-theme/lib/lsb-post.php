d7b455d6f225f62f2aa1502dca5b1b80bdaba47a
Benedicte Raae <raae@bgraphic.no>
2017-04-13 22:54:24 +0200

    Cache variable when extending

--- wp-content/themes/lsb-base-theme/lib/lsb-post.php
+++ wp-content/themes/lsb-base-theme/lib/lsb-post.php
@@ -2,32 +2,42 @@
 
 class LSB_Post extends TimberPost {
 
+	var $_read_more;
+	var $_sections;
+
 	public function lsb_read_more() {
-		$post_type_obj = get_post_type_object( $this->post_type );
-		if(isset($post_type_obj->labels->lsb_read_more)) {
-			return sprintf($post_type_obj->labels->lsb_read_more, $this->post_title );
-		} else {
-			return __('Les hele artikkelen', 'lsb');
+		if( !$this->_read_more ) {
+			$post_type_obj = get_post_type_object( $this->post_type );
+			if(isset($post_type_obj->labels->lsb_read_more)) {
+				$this->_read_more = sprintf($post_type_obj->labels->lsb_read_more, $this->post_title );
+			} else {
+				$this->_read_more = __('Les hele artikkelen', 'lsb');
+			}
 		}
+
+		return $this->_read_more;
 	}
 
 	public function lsb_sections() {
-		$post_sections = get_field('lsb_post_sections');
-		if(!$post_sections) {
-			$post_sections = array();
-		}
+		if( !$this->_sections ) {
+			$this->_sections = get_field('lsb_post_sections');
+			if( !$this->_sections) {
+				$this->_sections = array();
+			}
 
-		foreach ($post_sections as $key => &$section) {
-			$post_type = $section['acf_fc_layout'];
-			$section['post_type'] = $post_type;
+			foreach ($this->_sections as $key => &$section) {
+				$post_type = $section['acf_fc_layout'];
+				$section['post_type'] = $post_type;
 
-			if(post_type_exists($post_type)) {
-				$section['title'] = $section['lsb_title'];
-				$section['link'] = get_post_type_archive_link($post_type);
-				$section['subtitle'] = $section['lsb_subtitle'];
-				$section['posts'] = Timber::get_posts(array('post_type' => $post_type), LSB_Post::class);
+				if(post_type_exists($post_type)) {
+					$section['title'] = $section['lsb_title'];
+					$section['link'] = get_post_type_archive_link($post_type);
+					$section['subtitle'] = $section['lsb_subtitle'];
+					$section['posts'] = Timber::get_posts(array('post_type' => $post_type), LSB_Post::class);
+				}
 			}
 		}
-		return $post_sections;
+
+		return $this->_sections;
 	}
 }
