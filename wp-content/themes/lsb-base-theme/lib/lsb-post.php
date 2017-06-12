aa606290cdeb966919af813fa02858e92049e41a
Benedicte Raae <raae@bgraphic.no>
2017-06-12 13:04:54 +0200

    Create content from acf fields

--- wp-content/themes/lsb-base-theme/lib/lsb-post.php
+++ wp-content/themes/lsb-base-theme/lib/lsb-post.php
@@ -6,6 +6,25 @@ class LSB_Post extends TimberPost {
 	var $_read_more;
 	var $_sections;
 
+	public function content($page = 0, $len = -1) {
+		// var_dump($this);
+
+		if($this->post_type !== 'lsb_book') {
+			return parent::content($page, $len);
+		}
+
+		if( !$this->_content ) {
+			$lsb_review = $lsb_pre_reading = get_field_object( 'lsb_review' );
+			$lsb_quote = $lsb_pre_reading = get_field_object( 'lsb_quote' );
+
+			$this->_content = "";
+			$this->_content .= sprintf("<h2>%s</h2>%s", $lsb_review['label'], $lsb_review['value']);
+			$this->_content .= sprintf("<h2>%s</h2>%s", $lsb_quote['label'], $lsb_quote['value']);
+		}
+
+		return $this->_content;
+	}
+
 	public function read_more() {
 		if( !$this->_read_more ) {
 			$post_type_obj = get_post_type_object( $this->post_type );
