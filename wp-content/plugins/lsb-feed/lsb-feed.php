b35bd6c77c73e8cf5e134eb59ab4703e7dd5bf62
Benedicte Raae <raae@bgraphic.no>
2017-10-12 09:53:16 +0200

    Check if constant exists

--- wp-content/plugins/lsb-feed/lsb-feed.php
+++ wp-content/plugins/lsb-feed/lsb-feed.php
@@ -15,7 +15,7 @@ include('lib/lsb_book-content.php');
 add_action( 'rss2_item', __NAMESPACE__ . '\\add_featured_image_in_rss_2' );
 add_action( 'rss2_item', __NAMESPACE__ . '\\add_lsb_book_meta_in_rss' );
 
-if ( WP_ENV === 'development') {
+if ( defined( 'WP_ENV' ) && WP_ENV === 'development') {
   add_filter( 'http_request_args', __NAMESPACE__ . '\\set_reject_unsafe_urls_to_false');
 }
 
