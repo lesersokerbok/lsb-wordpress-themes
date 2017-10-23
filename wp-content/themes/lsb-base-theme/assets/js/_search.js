aed8c8a0f11720778b678bf03be2120c2ccb08d2
Benedicte Raae <raae@bgraphic.no>
2017-10-23 14:05:07 +0200

    Fix alogolia problem on feb2017

--- wp-content/themes/lsb-base-theme/assets/js/_search.js
+++ wp-content/themes/lsb-base-theme/assets/js/_search.js
@@ -1,15 +1,12 @@
-(function($) {
+(function ($) {
   if (typeof algolia === "undefined") {
+    console.log("Algolia is not set up properly");
     return;
   }
 
-  if (
-    algolia.indices.searchable_posts === undefined &&
-    jQuery(".admin-bar").length > 0
-  ) {
-    alert(
-      "It looks like you haven't indexed the searchable posts index. Please head to the Indexing page of the Algolia Search plugin and index it."
-    );
+  if (algolia.indices.searchable_posts === undefined) {
+    console.log("It looks like you haven't indexed the searchable posts index.");
+    return;
   }
 
   if (algolia.indices.searchable_posts.name.includes("wp_lsb_")) {
@@ -103,7 +100,7 @@
       facetingAfterDistinct: true,
       attributesToSnippet: ["lsb_review:20", "lsb_quote:20"]
     },
-    searchFunction: function(helper) {
+    searchFunction: function (helper) {
       console.log("Search", search.helper.state.query);
       var savedPage = helper.state.page;
       var isSearchPage = $("body").hasClass("search");
@@ -130,7 +127,7 @@
   });
 
   // Search box widget
-  $("#algolia-form input").each(function() {
+  $("#algolia-form input").each(function () {
     search.addWidget(
       instantsearch.widgets.searchBox({
         container: this,
@@ -147,7 +144,7 @@
       container: "#algolia-hits",
       hitsPerPage: 30,
       transformData: {
-        item: function(book) {
+        item: function (book) {
           addRelevantMetaAndContent(book);
           return book;
         }
@@ -176,8 +173,8 @@
   // Start
   search.start();
 
-  $searchInput = $("#algolia-form").each(function() {
-    $(this).bind("submit", function(e) {
+  $searchInput = $("#algolia-form").each(function () {
+    $(this).bind("submit", function (e) {
       e.preventDefault();
       $(this)
         .find("input")
